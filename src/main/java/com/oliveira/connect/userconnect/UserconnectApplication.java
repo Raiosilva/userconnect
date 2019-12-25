df4841972540ac5a71cdb2a387776f4e3665c98b
Raiosilva <raimundopi025@gmail.com>
2019-12-25 14:43:50 -0300

    first commit

---
+++ src/main/java/com/oliveira/connect/userconnect/UserconnectApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.connect.userconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveira/connect/userconnect/UserconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.connect.userconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
